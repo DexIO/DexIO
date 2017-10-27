40488a76624cf5c1b0d2abe90ccb5b51bff7ffb2
Germán Blasco <germanblasco@icloud.com>
2017-10-27 17:18:13 +0200

    color

--- src/renderer/components/inicio.vue
+++ src/renderer/components/inicio.vue
@@ -6,26 +6,53 @@
 <searchbar></searchbar>
 <div class="tarjetasInicio">
   <div class="col-7">
+  <router-link :to="{ name: 'pokedex', params: {} }">
     <div class="tarjetaPokedex">
-      <div class="col-3 imgInicio">
+      <div class="imgInicio">
         <img src="../assets/assets/img/pokedex.png" alt="">
       </div>
+      <div class="pokedexText">
+        <h2><span style="color: #ffffff">Pokédex</span></h2>
+      </div>
     </div>
+  </router-link>
   </div>
   <div class="col-5">
+    <router-link :to="{ name: 'editor', params: {} }">
     <div class="tarjetaEditor">
-
+      <div class="imgInicio">
+        <img src="../assets/assets/img/editor.png" alt="">
+      </div>
+      <div class="pokedexText">
+        <h2><span style="color: #ffffff">Editor</span></h2>
+      </div>
     </div>
+    </router-link>
   </div>
   <div class="col-5">
-    <div class="tarjetaEquipos">
+    <router-link :to="{ name: 'equipos', params: {} }">
 
+    <div class="tarjetaEquipos">
+      <div class="imgInicio">
+        <img src="../assets/assets/img/equipos.png" alt="">
+      </div>
+      <div class="pokedexText">
+        <h2><span style="color: #ffffff">Equipos</span></h2>
+      </div>
     </div>
+  </router-link>
   </div>
   <div class="col-7">
+    <router-link :to="{ name: 'calculadora', params: {} }">
     <div class="tarjetaCalculadora">
-
+      <div class="imgInicio">
+        <img src="../assets/assets/img/calculadora.png" alt="">
+      </div>
+      <div class="pokedexText">
+        <h2><span style="color: #ffffff">Calculadora</span></h2>
+      </div>
     </div>
+    </router-link>
   </div>
 </div>
 </div>
@@ -46,46 +73,63 @@ overflow-x: hidden;}
   margin-left: 70px;
   margin-right: 70px;
   margin-top: 10px;
-min-height:420px;
 }
 .tarjetaPokedex{
   width: 98%;
-  height: 11.5625em;
-  background: linear-gradient(90deg, #DF1515, #FC7272);
+height: 31vh;
+background-image: linear-gradient(45deg, #F5317F 0%, #FF7C6E 100%);
 border-radius: 12px;
 box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.25);
 overflow:visible;
-
+display: table;
 }
 .tarjetaEditor{
   width: 98%;
-  height: 11.5625em;
-  background: linear-gradient(90deg, #FF7406, #FFDF3D);
+height: 31vh;
+  background-image: linear-gradient(45deg, #FF8359 0%, #FFDF40 100%);
   border-radius: 12px;
   box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.25);
   float: right;
   overflow:visible;
+  display: table;
+
 
 }
 .tarjetaEquipos{
   width: 98%;
-  height: 11.5625em;
-  background: linear-gradient(90deg, #05745F, #3BDFD2);
+height: 31vh;
+  background-image: linear-gradient(45deg, #54D169 0%, #AFF57A 100%);
   border-radius: 12px;
   box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.25);
   margin-top: 10px;
+  margin-bottom: 25px;
   overflow:visible;
+  display: table;
+
 
 }
 .tarjetaCalculadora{
   width: 98%;
-  height: 11.5625em;
-  background: linear-gradient(90deg, #015FDF, #1ED2FC);
+height: 31vh;
+  background-image: linear-gradient(45deg, #1270E3 0%, #59C2FF 100%);
   border-radius: 12px;
   box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.25);
   float: right;
   margin-top: 10px;
+  margin-bottom: 25px;
   overflow:visible;
+  display: table;
+
 }
-.imgInicio{}
+.imgInicio{
+  padding-left:30pX;
+  display: table-cell;
+  border-spacing: 0px;
+  vertical-align: middle;
+  margin: 0;
+}
+.pokedexText{
+  display: table-cell;
+  vertical-align: middle;
+}
 </style>
